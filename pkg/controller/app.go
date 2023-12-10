8f1083a0297e19a0846423874d21bd36c46a9f6e
JohnnyOhms <Johnchinweike08@gmail.com>
2023-12-10 04:28:38 -0600

    add: routes controllers functionality

--- pkg/controller/app.go
+++ pkg/controller/app.go
@@ -8,7 +8,7 @@ import (
 	"github.com/JohnnyOhms/crud-server/pkg/config"
 	"github.com/JohnnyOhms/crud-server/pkg/model"
 	"github.com/JohnnyOhms/crud-server/pkg/utils"
-	_ "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 type Result struct {
@@ -63,34 +63,187 @@ func CreateInfo(w http.ResponseWriter, r *http.Request) {
 	w.Write(response)
 }
 
-func GetData(w http.ResponseWriter, r *http.Request) {
+func GetInfo(w http.ResponseWriter, r *http.Request) {
+	var userId string
+	if userId == "" || len(userId) == 0 {
+		http.Error(w, "authenticate to continue ", http.StatusUnauthorized)
+		return
+	}
 	w.Header().Set("Content-Type:", "aplicaton/json")
+
+	res, err := model.GetInfo(userId, config.Collection)
+	if err != nil {
+		http.Error(w, "failed to get data from database", http.StatusBadRequest)
+		fmt.Println("Error in getting data from db", err)
+		return
+	}
+	data := Result{
+		Success: true,
+		Mssg:    "info gotten successfully",
+		Data:    res,
+	}
+
+	result, err := utils.Encoding(data)
+	if err != nil {
+		http.Error(w, "failed to encode data", http.StatusInternalServerError)
+		fmt.Println("Error in encoding data", err)
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
-
+	w.Write(result)
 }
 
-func GetSingleData(w http.ResponseWriter, r *http.Request) {
-	// params := mux.Vars(r)
-	// Id := params["id"]
-	w.Header().Set("Content-Type:", "aplicaton/json")
+func GetSingleInfo(w http.ResponseWriter, r *http.Request) {
+	var userId string
+	if userId == "" || len(userId) == 0 {
+		http.Error(w, "authenticate to continue ", http.StatusUnauthorized)
+		return
+	}
+	params := mux.Vars(r)
+	if params["id"] == "" {
+		http.Error(w, "id param cannot be null", http.StatusBadRequest)
+		fmt.Println("null id param provided")
+		return
+	}
+	Id := params["id"]
+	res, err := model.GetSingleInfo(userId, Id, config.Collection)
+
+	if err != nil {
+		http.Error(w, "failed to get data from database", http.StatusBadRequest)
+		fmt.Println("Error in getting data from db", err)
+		return
+	}
+	data := Result{
+		Success: true,
+		Mssg:    "info gotten successfully",
+		Data:    res,
+	}
+
+	result, err := utils.Encoding(data)
+	if err != nil {
+		http.Error(w, "failed to encode data", http.StatusInternalServerError)
+		fmt.Println("Error in encoding data", err)
+		return
+	}
 	w.WriteHeader(http.StatusOK)
+	w.Write(result)
 
 }
 
-func EditData(w http.ResponseWriter, r *http.Request) {
+func EditInfo(w http.ResponseWriter, r *http.Request) {
+	var userId string
+	if userId == "" || len(userId) == 0 {
+		http.Error(w, "authenticate to continue ", http.StatusUnauthorized)
+		return
+	}
+
+	params := mux.Vars(r)
+	if params["id"] == "" {
+		http.Error(w, "id param cannot be null", http.StatusBadRequest)
+		fmt.Println("null id param provided")
+		return
+	}
+	Id := params["id"]
+
+	if r.Body == nil || r.ContentLength == 0 {
+		http.Error(w, "Empty request body", http.StatusBadRequest)
+		return
+	}
+	var update interface{}
+
+	updated := utils.ParseBody(r, &update)
+
+	result, err := model.EditInfo(userId, Id, updated, config.Collection)
+	if err != nil {
+		http.Error(w, "failed to update data from the data base", http.StatusBadRequest)
+		return
+	}
+
+	data := Result{
+		Success: true,
+		Mssg:    "info updated successfully",
+		Data:    result,
+	}
+
+	res, err := utils.Encoding(data)
+	if err != nil {
+		http.Error(w, "failed to encode data ", http.StatusInternalServerError)
+		fmt.Println("encoding data failed")
+		return
+	}
 	w.Header().Set("Content-Type:", "aplicaton/json")
 	w.WriteHeader(http.StatusCreated)
+	w.Write(res)
 
 }
 
-func DeleteSingleData(w http.ResponseWriter, r *http.Request) {
+func DeleteSingleInfo(w http.ResponseWriter, r *http.Request) {
+	var userId string
+	if userId == "" || len(userId) == 0 {
+		http.Error(w, "authenticate to continue ", http.StatusUnauthorized)
+		return
+	}
+
+	params := mux.Vars(r)
+	if params["id"] == "" {
+		http.Error(w, "id param cannot be null", http.StatusBadRequest)
+		fmt.Println("null id param provided")
+		return
+	}
+	Id := params["id"]
+
+	result, err := model.DeleteSingleInfo(userId, Id, config.Collection)
+	if err != nil {
+		http.Error(w, "failed to update data from the data base", http.StatusBadRequest)
+		return
+	}
+
+	data := Result{
+		Success: true,
+		Mssg:    "info updated successfully",
+		Data:    result,
+	}
+
+	res, err := utils.Encoding(data)
+	if err != nil {
+		http.Error(w, "failed to encode data ", http.StatusInternalServerError)
+		fmt.Println("encoding data failed")
+		return
+	}
+
 	w.Header().Set("Content-Type:", "aplicaton/json")
-	w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(res)
 
 }
 
-func DeleteData(w http.ResponseWriter, r *http.Request) {
+func DeleteInfo(w http.ResponseWriter, r *http.Request) {
+	var userId string
+	if userId == "" || len(userId) == 0 {
+		http.Error(w, "authenticate to continue ", http.StatusUnauthorized)
+		return
+	}
+	res, err := model.DeleteInfo(userId, config.Collection)
+	if err != nil {
+		http.Error(w, "failed to delete info", http.StatusInternalServerError)
+		return
+	}
+
+	data := Result{
+		Success: true,
+		Mssg:    "all infos deleted sucessfully",
+		Data:    res,
+	}
+
+	result, err := utils.Encoding(data)
+	if err != nil {
+		http.Error(w, "failed to encode info", http.StatusInternalServerError)
+		fmt.Println("encoding data failed")
+		return
+	}
+
 	w.Header().Set("Content-Type:", "aplicaton/json")
 	w.WriteHeader(http.StatusCreated)
-
+	w.Write(result)
 }
