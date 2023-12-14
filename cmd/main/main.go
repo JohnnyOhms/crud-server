2f75c8ca72d3949921fac83ed5e9260ab82e3ba9
JohnnyOhms <Johnchinweike08@gmail.com>
2023-12-14 05:08:46 -0600

    cors added

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/JohnnyOhms/crud-server/pkg/Doc"
 	Config "github.com/JohnnyOhms/crud-server/pkg/config"
 	"github.com/JohnnyOhms/crud-server/pkg/routes"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -16,11 +18,22 @@ func main() {
 	routes.Router(r)
 	r.HandleFunc("/", Doc.Documentation).Methods("GET")
 
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
+
+	corsRouter := handlers.CORS(headersOk, originsOk, methodsOk)(r)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	if _, err := Config.ConnectDB(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("server running on port 8080\n")
-	if err := http.ListenAndServe(":8080", r); err != nil {
+	fmt.Printf("Server listening on port %s...\n", port)
+	if err := http.ListenAndServe(":"+port, corsRouter); err != nil {
 		log.Fatal(err)
 	}
 }
